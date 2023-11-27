3d815c228e07735c241de3400ed45f239ab6ed88
Zyruvias <zerianledrias@gmail.com>
2023-11-27 13:02:11 -0500

    initial upload

---
+++ Mods/CollatedHades/CollatedHades.lua
@@ -0,0 +1,16 @@
+--[[
+    Collated Hades
+    - Author: Zyruvias
+    - dependencies
+        - ModUtil 2.8.x
+        - ErumiUILib
+        - ZyrUvIas Util
+]]--f
+
+ModUtil.Mod.Register("CollatedHades")
+local config = {
+    Enabled = true,
+    NumRuns = 2,
+    SelectionBehavior = "Linear",
+}
+CollatedHades.config = config
